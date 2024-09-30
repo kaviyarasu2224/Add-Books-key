be9c2a61cb2a11edcf0778ac8136f2a2ac721aca
Kaviyarasu P <kaviyarasu2224@gmail.com>
2024-09-30 21:55:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ addbutton.addEventListener("click",function(){
    var maincontainer=document.querySelector(".container")
 
    var addbook=document.getElementById("addbook")
+
    var h2=document.getElementById("book-title")
    var h5=document.getElementById("book-author")
    var pera=document.getElementById("book-text")
